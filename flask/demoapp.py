4807d63570e4efb725168b5a5aeda0c24751f44d
tom <iamnewhand@163.com>
2017-06-25 23:34:48 +0800

    0625

--- flask/demoapp.py
+++ flask/demoapp.py
@@ -2,8 +2,17 @@
 # _*_ coding: utf-8 _*_
 
 from flask import Flask
-import
-import
-Flask(__name__).run()
 
+app = Flask(__name__)
+
+#装饰器
+@app.route('/')
+def index():
+    a = 3
+    b = 'c'
+    d = 4
+    return 'hello world'+123
+print __name__
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000,debug=True)
 
